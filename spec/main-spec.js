b17b37604de00a8e077725b639e4f68d1b000b5b
Joe Fitzgerald <jfitzgerald@pivotal.io>
2015-12-06 11:03:39 -0700

    Add Package Skeleton That Uses go-config

---
+++ spec/main-spec.js
@@ -0,0 +1,30 @@
+'use babel'
+
+describe('gorename', () => {
+  let mainModule = null
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage('go-config').then(() => {
+        return atom.packages.activatePackage('gorename')
+      }).then((pack) => {
+        mainModule = pack.mainModule
+        console.log(mainModule)
+      })
+    })
+
+    waitsFor(() => {
+      return mainModule.getGoconfig() !== false
+    })
+  })
+
+  describe('when the gorename package is activated', () => {
+    it('activates successfully', () => {
+      expect(mainModule).toBeDefined()
+      expect(mainModule).toBeTruthy()
+      expect(mainModule.getGoconfig).toBeDefined()
+      expect(mainModule.consumeGoconfig).toBeDefined()
+      expect(mainModule.getGoconfig()).toBeTruthy()
+    })
+  })
+})
